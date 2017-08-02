3b5603023d65d50559321d1d008869964e343209
Valery Litvinchuk <valery.litvinchuk@gmail.com>
2017-08-02 16:36:03 +0300

    added slideshow

--- app/js/common.js
+++ app/js/common.js
@@ -25,4 +25,27 @@ $(function() {
 		return false;
 	});
 
+	// var slideNow = 1;
+	// var slideCount = $('.slidewrapper').children().length;
+	// var translateWidth = 0;
+	// var slideInterval = 2000;
+
+	// function nextSlide() {
+	// 	if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+	// 		$('.slidewrapper').css('transform', 'translate(0, 0)');
+	// 		slideNow = 1;
+	// 	} else {
+	// 		translateWidth = -$('.viewport').width() * (slideNow);
+	// 		$('.slidewrapper').css({
+ //            'transform': 'translate(' + translateWidth + 'px, 0)',
+ //            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+ //            '-ms-transform': 'translate(' + translateWidth + 'px, 0)'});
+	// 		slideNow++;
+	// 	}
+	// }
+
+	// setInterval(nextSlide, slideInterval);
+
+	// console.log(slideCount);
+
 });
